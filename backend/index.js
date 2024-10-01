6610919bc3b58a8042caff03eed147416321169b
Pavan <pavan@gmail.com>
2024-10-01 18:19:23 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://project-e4dd-p8mg.vercel.app/",
     methods: ["POST", "GET"],
     credentials: true,
   })
